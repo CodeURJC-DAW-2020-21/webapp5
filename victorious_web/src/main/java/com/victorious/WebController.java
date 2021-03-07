f9b5f9cfc11d9262fb31d602ac94728ebf0ce1e4
kikegg <quique23d@hotmail.com>
2021-03-07 20:03:56 +0100

    Header + Footer + 1st Controller
    Split header and footer.
    First steps in web controler.
    WARNING: header has token problem.

---
+++ victorious_web/src/main/java/com/victorious/WebController.java
@@ -0,0 +1,91 @@
+package com.victorious;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class WebController {
+	
+	
+	@RequestMapping(value = {"/", "index"} )
+	public String index(Model model){
+		
+		return "index";
+	}
+	
+	@RequestMapping("/tournaments")
+	public String tournaments(){
+		
+		return "tournaments";
+	}	
+	
+	@RequestMapping("/tournaments/tournament")
+	public String tournament(Model model){
+		
+		return "tournament";
+	}	
+	
+	@RequestMapping("/teams")
+	public String teams(Model model){
+		
+		return "teams";
+	}	
+	
+	@RequestMapping("/teams/team")
+	public String team(Model model){
+		
+		return "team";
+	}
+	
+	@RequestMapping("/league")
+	public String league(Model model){
+		
+		return "league";
+	}
+	
+	@RequestMapping("/contact")
+	public String contact(Model model){
+		
+		return "contact";
+	}
+	
+	@RequestMapping("/login")
+	public String login(Model model){
+		
+		return "login";
+	}
+	
+	@RequestMapping("/settings")
+	public String settings(Model model){
+		
+		return "settings";
+	}
+	
+	@RequestMapping("/sign-up")
+	public String signUp(Model model){
+		
+		return "sign-up";
+	}
+	
+	@RequestMapping("/user")
+	public String user(Model model){
+		
+		return "user";
+	}
+	
+	@RequestMapping("/newTournament")
+	public String newTournament(Model model){
+		
+		return "newTournament";
+	}
+	
+	@RequestMapping("/newTeam")
+	public String newTeam(Model model){
+		
+		return "newTeam";
+	}
+	
+}
