cd35e8ad8de8f00a3ddb17ec168d42027da1237e
Jacobo Miguel Blanzaco <blanzacojacobo@gmail.com>
2021-04-03 18:45:38 +0200

    Contact Mail System
    Implementation of the mail system for contact with spring mail.

--- victorious_web/src/main/java/com/victorious/WebController.java
+++ victorious_web/src/main/java/com/victorious/WebController.java
@@ -4,14 +4,22 @@ import java.security.Principal;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
+	
+	@Autowired
+	private JavaMailSender mailSender;
 	
 	@ModelAttribute
 	public void addAttributes(Model model, HttpServletRequest request) {
@@ -42,10 +50,33 @@ public class WebController {
 		return "league";
 	}
 	
-	@RequestMapping("/contact")
-	public String contact(Model model){
-		
+	@GetMapping("/contact")
+	public String showContactForm(Model model){
 		return "contact";
+	}
+	
+	@PostMapping("/contact")
+	public String submitMessage(HttpServletRequest  request,
+			@RequestParam String name,
+			@RequestParam String email,
+			@RequestParam String subject,
+			@RequestParam String content) {
+		
+		SimpleMailMessage message = new SimpleMailMessage();
+		
+		message.setFrom("SpringMail@gmail.com");
+		message.setTo("VictoriusWeb@gmail.com");
+		String mailSubject = name +"has sent a message";
+		String mailContent = "Sender Name: " + name + "\n"
+							+"Sender Email: " + email + "\n"
+							+"Message Subject: " + subject +"\n"
+							+ "--------------" + "\n"
+							+ content;
+		message.setSubject(mailSubject);
+		message.setText(mailContent);
+		
+		mailSender.send(message);
+		return  "contact";
 	}
 	
 	@RequestMapping("/newTournament")
